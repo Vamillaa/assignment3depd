65311867ba7a4ac8a274345a01f9aa092a1b0a9c
Vamillaa <147577977+Vamillaa@users.noreply.github.com>
2023-12-09 11:29:19 +0700

    Add files via upload

---
+++ lib/services/services.dart
@@ -0,0 +1,7 @@
+import 'dart:convert';
+
+import 'package:depd_23api/shared/shared.dart';
+import 'package:http/http.dart' as http;
+import 'package:depd_23api/models/models.dart';
+
+part 'masterdataservice.dart';
---
+++ lib/shared/const.dart
@@ -0,0 +1,6 @@
+part of 'shared.dart';
+
+class Const {
+  static String baseUrl = 'api.rajaongkir.com';
+  static String apiKey = 'dd48f33dc078e531fdd79335645aef2f';
+}
